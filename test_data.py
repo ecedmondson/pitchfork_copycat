70bdebddafbe8df1a7c12a4deb1e176fb46c532b
Thomas Banghart <banghart@flip3.engr.oregonstate.edu>
2020-02-17 12:28:19 -0800

    fixed untracked files

---
+++ test_data.py
@@ -0,0 +1,84 @@
+artists = [
+    {
+        "id" : 1,
+        "name" : "Trans Am",
+        "website" : "https://thrilljockey.com/artists/trans-am-272f31c5-335d-48cd-9c1c-b0c8e6bf9b23",
+        "image" : "https://www.villagevoice.com/wp-content/uploads/2011/10/transam_promo.jpg",
+        "location" : "Maryland, USA",
+        "description": "Three-piece band that was one of the originators of \"post-rock\" in the mid 1990s. Their work centers on a cerebrally robotic, usually instrumental, semi-danceable, and minimalistic format."
+    },
+    {
+        "id" : 2,
+        "name" : "Brain Blade",
+        "website" : "http://www.brianblade.com/",
+        "image" : "https://upload.wikimedia.org/wikipedia/commons/5/56/Brian_Blade_at_MJF_2014.jpg",
+        "location" : "Louisiana, USA",
+        "description": " American jazz drummer, composer, session musician, and singer-songwriter."
+    }
+]
+
+test_albums = [
+    {
+        "id" : 1,
+        "artist_id" : 1,
+        "title" : "VolumeX",
+        "album_cover" : "https://media.pitchfork.com/photos/5929a74ab1335d7bf169900b/1:1/w_320/a03baf46.jpg",
+        "release_date" : "May 19th, 2014",
+        "publisher" : "Thrill Jockey",
+        "spotify_url": "https://open.spotify.com/album/68k5DgjivT5mLK3U3zyOKp",
+    },
+    {
+        "id" : 2,
+        "artist_id" : 2,
+        "title" : "Landmarks",
+        "album_cover" : "https://images-na.ssl-images-amazon.com/images/I/51WusIzhL-L._SY355_.jpg",
+        "release_date" : "August 12th, 2014",
+        "publisher" : "Blue Note",
+        "spotify_url" : "https://open.spotify.com/album/5InHDZVvNbDDvq917BmMgT"
+    }
+]
+
+reviews = [
+    {
+        "id" : 1,
+        "review_text" : "Pretty good! Like the drums.",
+        "rating" : 8.3,
+        "user_id" : 1,
+        "created_time" : "13:59:30"
+    },
+    {
+        "id" : 2,
+        "review_text" : "Wonderful! The very best!",
+        "rating" : 10.0,
+        "user_id" : 2,
+        "created_time" : "16:59:30"
+    }
+]
+
+users = [
+    {
+        "id" : 1,
+        "first_name" : "Bob",
+        "last_name" : "Jon",
+        "email" : "bobJ@gmail.com",
+        "created_date" : "2019-02-06"
+    },
+    {
+        "id" : 2,
+        "first_name" : "Olivia",
+        "last_name" : "Arstein",
+        "email" : "oa@gmail.com",
+        "created_date" : "2019-02-08"
+    }
+]
+
+genres = [
+    {
+        "id" : 1,
+        "description" : "Rock"
+    },
+    {
+        "id" : 2,
+        "description" : "Jazz"
+    }
+]
