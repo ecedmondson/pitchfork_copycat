d1bd005db6e8b05ae21eedb35909518fe135f339
Emily Edmondson <ecedmondson@gmail.com>
2020-03-04 16:30:23 -0800

    ENHANCEMENT: must create user apart from review page

--- app.py
+++ app.py
@@ -135,15 +135,17 @@ def route_search(search_type, search_desired):
 def review_page(album, artist):
     form = ReviewForm()
     if form.is_submitted():
-        # TODO: validate data
         user_id = users.get_user_id_from_names_and_email(
             firstname=form.firstname.data,
             lastname=form.lastname.data,
             email=form.email.data,
         )
-        album_id = albums.get_album_id_from_name(name=_readable_syntax(album))
-        # TODO: add a real rating system in
-        reviews.add_new_review(form.body.data, "10", str(user_id), str(album_id))
+        if user_id:
+            album_id = albums.get_album_id_from_name(name=_readable_syntax(album))
+            # TODO: add a real rating system in
+            reviews.add_new_review(form.body.data, "10", str(user_id), str(album_id))
+        else:
+           flash("User not found")
     return render_template(
         "review_page.html",
         form=form,
@@ -166,4 +168,4 @@ def add_album_page():
 	return render_template("add_album.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5162)
+    app.run(debug=True, host="0.0.0.0", port=4740)
