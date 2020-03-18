b8de7609649963832b7ad6f8842f247a6fc695a6
Emily Edmondson <ecedmondson@gmail.com>
2020-03-17 21:01:03 -0500

    Commenting app.py for graders

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ app.config["SECRET_KEY"] = "derp"
 
 Bootstrap(app)
 
+#################
+# Table Objects #
+#################
+
 albums = AlbumTable()
 artist = ArtistTable()
 reviews = ReviewTable()
@@ -23,15 +27,27 @@ users = UserTable()
 search = SearchSQL()
 genres = GenreTable()
 
+###########
+# Helpers #
+###########
+
+# This section contains functions which help perform
+
 def _route_syntax(value):
+    """Take data and format it so that it can be easily read in a route for a URL."""
     return value.lower().replace(" ", "_")
 
-
 def _readable_syntax(route):
+    """Take a URL route and make it easily useable as data to interact with backend."""
     return " ".join([x.capitalize() for x in route.split("_")])
+
+##################
+# Error Handling #
+##################
 
 @app.errorhandler(HTTPException)
 def handle_werk(e):
+    """Route Werkzeug Programming Errors to 500 server error."""
     response = e.get_response()
     response.data = json.dumps({
         "code": e.code,
@@ -50,20 +66,24 @@ def not_found(e):
 def server_error(e):
     return render_template('500.html', error=e)
 
+##################################
+# Routing and Template Rendering #
+##################################
 
 @app.route("/", methods=("GET", "POST"))
 def home():
+    """Renders the home/main/index/etc. page and handles POST request to the search form."""
     form = SearchForm()
     if form.is_submitted():
         if form.select_search.data:
-            print(form.search_keyword.data)
-            print(form.select_search.data)
+            # Check to see if search in database
             search_results = search.execute_search(
                 form.search_keyword.data, form.select_search.data
             )
-            print(search_results)
+        # If search in DB: 
         if search_results:
             result_length, query = search_results
+	    # Route for exact match
             if result_length == 1:
                 return render_template(
                     must_get_key(
@@ -78,11 +98,13 @@ def home():
                     query=only_item_of(query),
                     subquery=[],
                 )
+	    # Route for multiple search matches
             return render_template(
                 "too_many_search_results.html",
                 query=query,
                 search_type=form.select_search.data,
             )
+        # Route for no matches
         return render_template(
             "search_results_none.html", search_type=form.select_search.data
         )
@@ -94,6 +116,7 @@ def home():
 
 @app.route("/edit_album", methods=("GET", "UPDATE"))
 def edit_album():
+    """API endpoint to handle editing the album image. No template. Handles form on edit album page."""
     if request.method == 'GET':
        data = request.args.to_dict()
        if not data['album-art']:
@@ -107,6 +130,7 @@ def edit_album():
 
 @app.route("/delete_album", methods=("GET", "POST"))
 def delete_album():
+    """API endpoint to handle deleting an album. No template. Handles form on edit album page."""
     if request.method == 'GET':
         data = request.args.to_dict()
         if data['delete-album'] == 'delete':
@@ -116,17 +140,20 @@ def delete_album():
 
 @app.route("/route_to_review_page/<album>/<artist>", methods=("GET", "POST"))
 def route(album, artist):
+    """Route to review page after user clicks on link."""
     return redirect(
         url_for("review_page", album=_route_syntax(album), artist=_route_syntax(artist))
     )
 
 @app.route("/artists")
 def all_artists():
+    """Renders the view all artists page."""
     artists = artist.all_artists()
     return render_template("all_artists.html", artists=artists)
 
 @app.route("/create_user", methods=("GET", "POST"))
 def create_new_user():
+   """Renders the create user form/page and handles POST requests to create form."""
    valid_email_endings = ['.com', '.net', '.org', '.edu'] 
    if request.method == 'POST':
         data = request.form.to_dict()
@@ -142,16 +169,19 @@ def create_new_user():
 
 @app.route("/artists/<artist_name>", methods=("GET", "POST"))
 def route_single_artist_page(artist_name):
+    """Handles routing to single artist page when clicked from button (especially in grid)."""
     artist_id = artist._select_artist_id_from_name(artist_name)
     single_artist = artist._select_single_artist_page(artist_id)
     albums = artist._select_albums_from_artist(artist_id)
     return render_template("single_artist_page.html", query=single_artist, albums=albums)
 
-
+# Update/Delete Context. This list stores data to help render the appropriate
+# comment for editing/deletion when a request is made to edit or delete
+# a comment from a given reviews page.
 ud_context = {}
 
 def _parse_edit_review_content(content):
-    print(content.split("-"))
+    """Helper takes content from a UI field and parses it for rendering in the edit review comment page."""
     album, artist, review_id = content.split("-")
     return {
         "album": album,
@@ -185,6 +215,7 @@ def delete_review_comment(**kwargs):
 
 @app.route("/route_to_edit/<page>/<validator>/<content>", methods=("GET", "POST"))
 def route_to_edit_page(page, validator, content):
+    """Route to edit a review comment page based on which comment is being reviewed."""
      kwargs = {"user_id": validator, "content": content}
      uuid_ = uuid.uuid4()
      ud_context[str(uuid_)] = kwargs
@@ -192,6 +223,7 @@ def route_to_edit_page(page, validator, content):
 
 @app.route("/edit_review_comment/<uuid>", methods=("GET", "POST"))
 def edit_review_comment(uuid, **kwargs):
+    """Renders the edit review comment form/page and handled POST requests to edit a review comment."""
     context = ud_context[uuid]
     user_id = context['user_id']
     content = _parse_edit_review_content(context['content'])
@@ -222,17 +254,24 @@ def route_add_album_page():
     "/route_to_search_results/<search_type>/<search_desired>", methods=("GET", "POST")
 )
 def route_search(search_type, search_desired):
-    # The main search will handle this if specific enough
-    # data is provided. If data returns multiple results, the app
-    # will redirect to a page providing the user with links to the
-    # various results. Those links are routed here, where the search is
-    # completed, hence the search table name from "main" or "redirect".
+    """Handling user input after multiple search results.
+
+    When multiple search results are found after querying the DB, this routing
+    function handles further user input against the multiple results template
+    by allowing the user to choose a specific result and navigating to the appropriate
+    page.
+    """
     table = must_get_key({
 			"artist": artist,
                         "album": albums,
                         "user": users,
                         "genre": genres,
                         }, search_type)
+    # The main search will handle this if specific enough
+    # data is provided. If data returns multiple results, the app
+    # will redirect to a page providing the user with links to the
+    # various results. Those links are routed here, where the search is
+    # completed, hence the search table name from "main" or "redirect".
     length, full_search = search.complete_full_search_from_main_or_redirect(table, search_desired)
     return render_template(
                     must_get_key(
@@ -250,6 +289,7 @@ def route_search(search_type, search_desired):
 
 @app.route("/reviews/<album>/<artist>", methods=("GET", "POST"))
 def review_page(album, artist):
+    """Renders the reviews page for a given album and handles POST requests to submit a new review of an album."""
     form = ReviewForm()
     if form.is_submitted():
         user_id = users.get_user_id_from_names_and_email(
@@ -259,7 +299,6 @@ def review_page(album, artist):
         )
         if user_id:
             album_id = albums.get_album_id_from_name(name=_readable_syntax(album))
-            # TODO: add a real rating system in
             reviews.add_new_review(form.body.data, form.review_select.data, str(user_id), str(album_id))
         else:
            flash("User not found")
@@ -273,8 +312,9 @@ def review_page(album, artist):
         artist=_readable_syntax(artist),
     )
 
-@app.route("/new_genre", methods=(["POST"]))
+@app.route("/new_genre", methods=(["POST"]))		
 def new_genre():
+    """API endpoint for handling a new genre. No templates. Handles form on add album/artist pages."""
     id = genres.insert_genre(json.loads(request.data))
     if id > 0:
         flash("Genre successfully added!")
@@ -284,6 +324,7 @@ def new_genre():
 
 @app.route("/add_artist", methods=("GET", "POST"))
 def add_artist_page():
+   """Renders the add artist form page and handles POST request to create a new artist."""
     genre_all = genres.select_all_genres()
     if request.method == 'POST':
         data = request.form.to_dict()
@@ -297,6 +338,7 @@ def add_artist_page():
 
 @app.route("/add_album", methods=("GET", "POST"))
 def add_album_page():
+   """Renders the add album form page and handles POST request to create a new album."""
     genre_all = genres.select_all_genres()
     artist_all = artist.all_artists()
     if request.method == 'POST':
@@ -309,5 +351,9 @@ def add_album_page():
             return redirect(url_for("home"))
     return render_template("add_album.html", genres=genre_all, artists=artist_all)
 
+#######
+# Run #
+#######
+		 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5151)
+    app.run(debug=True, host="0.0.0.0", port=4740)
